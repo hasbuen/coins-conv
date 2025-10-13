348f88dde506d04e27e69abadd158b090a478458
Cesar Bueno <91395027+hasbuen@users.noreply.github.com>
2025-10-12 23:03:31 -0300

    Update Currency.vue

--- src/views/Currency.vue
+++ src/views/Currency.vue
@@ -1,7 +1,1 @@
-<template>
-  <v-app>
-    <v-main>
-      <CartaoParaCotacoes />
-    </v-main>
-  </v-app>
-</template>
+<template> <v-app> <v-main> <CartaoParaCotacoes v-for="(item, index) in lista" :key="index" :ref="'node' + index" :currency="item.currency" /> </v-main> </v-app> </template> <script> import CartaoParaCotacoes from '@/components/CartaoParaCotacoes.vue' export default { components: { CartaoParaCotacoes }, data () { return { lista: [ { currency: 'USD' }, { currency: 'BTC' }, { currency: 'EUR' } ] } } } </script>
